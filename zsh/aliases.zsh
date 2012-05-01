35047dd70ca066b3f9b1cc024036a8deac485532
Nick Nisi <nick@nisi.org>
2012-04-30 21:39:22 -0500

    Adding zsh config

---
+++ zsh/aliases.zsh
@@ -0,0 +1,25 @@
+# Filesystem aliases
+alias ..='cd ..'
+alias ...='cd ../..'
+alias l='ls -lah'
+alias la='ls -AF'
+alias ll='ls -lFh'
+alias lld='ls -l | grep ^d'
+alias rmf='rm -rf'
+
+# Helpers
+alias grep='grep --color=auto'
+alias df='df -h' # disk free, in Gigabytes, not bytes
+alias du='du -h -c' # calculate disk usage for a folder
+
+# git aliases
+alias g='git'
+alias ga='git add'
+alias gb='git branch'
+alias gl='git pull'
+alias gp='git push'
+alias gst='git s'
+alias gs='git stash save'
+alias gsp='git stash pop'
+alias gmv='git mv'
+alias grm='git rm'
