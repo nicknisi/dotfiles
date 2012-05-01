e387343082687fefd460720b7e016c45a8cefa88
Nick Nisi <nick@nisi.org>
2012-04-30 23:26:18 -0500

    Moving git aliases over to git dir

---
+++ git/aliases.zsh
@@ -0,0 +1,12 @@
+# git aliases
+alias g='git'
+alias ga='git add'
+alias gb='git branch'
+alias gl='git pull'
+alias gp='git push'
+alias gst='git s'
+alias gs='git stash save'
+alias gsp='git stash pop'
+alias gmv='git mv'
+alias grm='git rm'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -12,14 +12,3 @@ alias grep='grep --color=auto'
 alias df='df -h' # disk free, in Gigabytes, not bytes
 alias du='du -h -c' # calculate disk usage for a folder
 
-# git aliases
-alias g='git'
-alias ga='git add'
-alias gb='git branch'
-alias gl='git pull'
-alias gp='git push'
-alias gst='git s'
-alias gs='git stash save'
-alias gsp='git stash pop'
-alias gmv='git mv'
-alias grm='git rm'
