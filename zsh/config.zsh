eb096ce4bfb6f907f718f78c87ea0ca67ac22ae9
Nick Nisi <nick@nisi.org>
2012-04-30 23:40:24 -0500

    set defaults, set defaults PS1 (temp)

---
+++ zsh/config.zsh
@@ -0,0 +1,28 @@
+# make terminal command navigation sane again
+bindkey '^[^[[D' backward-word
+bindkey '^[^[[C' forward-word
+bindkey '^[[5D' beginning-of-line
+bindkey '^[[5C' end-of-line
+bindkey '^[[3~' delete-char
+bindkey '^[^N' newtab
+bindkey '^?' backward-delete-char
+
+setopt NO_BG_NICE
+setopt NO_HUP
+setopt NO_LIST_BEEP
+setopt LOCAL_OPTIONS
+setopt LOCAL_TRAPS
+setopt IGNORE_EOF
+setopt PROMPT_SUBST
+
+# history
+setopt HIST_VERIFY
+setopt EXTENDED_HISTORY
+setopt HIST_REDUCE_BLANKS
+setopt HIST_IGNORE_ALL_DUPS
+setopt INC_APPEND_HISTORY SHARE_HISTORY
+setopt APPEND_HISTORY
+
+setopt COMPLETE_ALIASES
+
+export PS1='%3~ ☠ '
