1313ed78eef954d7950deb94dfaf888299199802
Nick Nisi <nick@nisi.org>
2012-05-04 15:03:22 -0500

    Renamed git/aliases.zsh to git/git.zsh
    + Added a function to the file to allow for easier renaming of files
    
    Changing the case of a filename is difficult in git because git does not
    track case-sensitivity changes to a filename. I added a function which
    simply does a `git mv` got a temporary file name and then does a `git
    mv` to the desired filename.

--- git/git.zsh
+++ git/git.zsh
@@ -9,4 +9,10 @@ alias gs='git stash save'
 alias gsp='git stash pop'
 alias gmv='git mv'
 alias grm='git rm'
+alias grn='git_rename'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+
+function git_rename() {
+    git mv $1 "${2}-"
+    git mv "${2}-" $2
+}
