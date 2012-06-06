d09e4140aa0bd9e86292ef221ba1194c7e801d6c
Nick Nisi <nick@nisi.org>
2012-06-06 10:21:45 -0500

    Fix for rake and zsh
    This became an issue when I noticed I was getting an error running
    octopress tasks such as `rake new_post["foo"]`

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -12,5 +12,8 @@ alias grep='grep --color=auto'
 alias df='df -h' # disk free, in Gigabytes, not bytes
 alias du='du -h -c' # calculate disk usage for a folder
 
+# rake fix
+alias rake="noglob rake"
+
 # Applications
 alias mou='open -a Mou.app'
