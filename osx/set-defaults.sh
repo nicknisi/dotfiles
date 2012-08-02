130446f2976397066bd236eb6572494adb9ac367
Nick Nisi <nick@nisi.org>
2012-08-01 20:21:52 -0500

    started working on osx script

---
+++ osx/set-defaults.sh
@@ -0,0 +1,13 @@
+## These are a work in progress
+
+# show hidden files by default
+defaults write com.apple.Finder AppleShowAllFiles -bool true
+
+# only use UTF-8 ub Terminal.app
+defaults write com.apple.terminal StringEncodings -array 4
+
+# expand save dialog by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+# disable resume system wide
+# defaults write NSGlobalDomainNSQuitAlwaysKeepWindows -bool false
