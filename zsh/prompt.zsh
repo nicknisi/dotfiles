528ac51d76f0340f66956568bf2885b29c72c7bb
Nick Nisi <nick@nisi.org>
2012-08-12 19:49:59 -0500

    Updating prompt symbols

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -12,9 +12,9 @@ git_dirty() {
   else
     if [[ $st == "nothing to commit (working directory clean)" ]]
     then
-      echo "%{$fg_bold[green]%}✔ $(git_prompt_info)%{$reset_color%}"
+      echo "%{$fg_bold[green]%}☺ $(git_prompt_info)%{$reset_color%}"
     else
-      echo "%{$fg_bold[red]%}✘ $(git_prompt_info)%{$reset_color%}"
+      echo "%{$fg_bold[red]%}☠ $(git_prompt_info)%{$reset_color%}"
     fi
   fi
 }
