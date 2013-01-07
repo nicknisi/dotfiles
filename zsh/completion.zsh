43d51d674d270a5d78ece601aaca2ee582b13f93
Nick Nisi <nick@nisi.org>
2013-01-07 08:44:11 -0600

    Default completion to files
    This way, completion still works for aliases

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,6 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# default to file completion
+zstyle ':completion:*' completer _expand _complete _files _correct _approximate
