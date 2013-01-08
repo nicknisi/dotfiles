a80c5a4ccd3231697a7e4251e4a64ff77b105daa
Nick Nisi <nick@nisi.org>
2013-01-07 19:27:12 -0600

    Updating which script is used for git-completion

--- git/completion.sh
+++ git/completion.sh
@@ -1,4 +1,5 @@
-completion=/usr/local/etc/bash_completion.d/git-completion.bash
+# this is specific to the location of the current version of git, installed by homebrew
+completion=/usr/local/Cellar/git/1.8.0.1/etc/bash_completion.d/git-completion.bash
 
 if test -f $completion; then
     source $completion
