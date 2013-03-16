a2deda11071d11270fd6775e14e37d21d918bf5c
Nick Nisi <nick@nisi.org>
2013-03-16 06:59:44 -0500

    Removing git function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -35,19 +35,6 @@ function server() {
     python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port"
 }
 
-# git log with per-commit cmd-clickable GitHub URLs (iTerm)
-# function gf() {
-#   local remote="$(git remote -v | awk '/^origin.*\(push\)$/ {print $2}')"
-#   [[ "$remote" ]] || return
-#   local user_repo="$(echo "$remote" | perl -pe 's/.*://;s/\.git$//')"
-#   git log $* --name-status --color | awk "$(cat <<AWK
-#     /^.*commit [0-9a-f]{40}/ {sha=substr(\$2,1,7)}
-#     /^[MA]\t/ {printf "%s\thttps://github.com/$user_repo/blob/%s/%s\n", \$1, sha, \$2; next}
-#     /.*/ {print \$0}
-# AWK
-#   )" | less -F
-# }
-
 # take this repo and copy it to somewhere else minus the .git stuff.
 function gitexport(){
     mkdir -p "$1"
