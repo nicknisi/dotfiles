9c7b322645b03355850594e2d4f71c09af899592
Nick Nisi <nick@nisi.org>
2013-08-04 17:41:00 -0500

    Clean up tmux theme and status info

---
+++ tmux/theme.sh
@@ -0,0 +1,59 @@
+#### COLOUR
+
+tm_color_active=colour201
+tm_color_inactive=colour11
+tm_color_feature=colour202
+
+# separators
+tm_separator_left_bold="◀"
+tm_separator_left_thin="❮"
+tm_separator_right_bold="▶"
+tm_separator_right_thin="❯"
+
+set -g status-left-length 32
+set -g status-right-length 150
+set -g status-interval 5
+
+
+# default statusbar colors
+# set-option -g status-bg colour0
+set-option -g status-fg $tm_color_active
+set-option -g status-bg default
+set-option -g status-attr default
+
+# default window title colors
+set-window-option -g window-status-fg $tm_color_inactive
+set-window-option -g window-status-bg default
+set -g window-status-format "#I #W"
+
+# active window title colors
+set-window-option -g window-status-current-fg $tm_color_active
+set-window-option -g window-status-current-bg default
+set-window-option -g  window-status-current-format "#[bold]#I #W"
+
+# pane border
+set-option -g pane-border-fg $tm_color_inactive
+set-option -g pane-active-border-fg $tm_color_active
+
+# message text
+set-option -g message-bg default
+set-option -g message-fg $tm_color_active
+
+# pane number display
+set-option -g display-panes-active-colour $tm_color_active
+set-option -g display-panes-colour $tm_color_inactive
+
+# clock
+set-window-option -g clock-mode-colour $tm_color_active
+
+tm_spotify='#[fg=$tm_color_feature]#(osascript ~/.dotfiles/applescripts/spotify.scpt)'
+tm_itunes='#[fg=$tm_color_feature]#(osascript ~/.dotfiles/applescripts/itunes.scpt)'
+tm_rdio='#[fg=$tm_color_feature]#(osascript ~/.dotfiles/applescripts/rdio.scpt)'
+tm_battery='#(~/.dotfiles/bin/battery_indicator.sh)'
+
+tm_date='#[fg=$tm_color_inactive] %R %d %b'
+tm_host='#[fg=colour202,bold]#h'
+tm_session_name="#[fg=$tm_color_feature,bold]$tm_separator_right_thin #S"
+
+set -g status-left $tm_session_name
+set -g status-right $tm_itunes' '$tm_rdio' '$tm_battery' '$tm_date
