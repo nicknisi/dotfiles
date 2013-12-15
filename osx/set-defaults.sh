9aa7ca0cb10ee357b61967bed31148441f20ec52
Nick Nisi <nick@nisi.org>
2013-12-15 13:30:15 -0600

    Update osx defaults script

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -1,7 +1,10 @@
 ## These are a work in progress
 
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
 # show hidden files by default
-defaults write com.apple.Finder AppleShowAllFiles -bool true
+defaults write com.apple.Finder AppleShowAllFiles -bool false
 
 # only use UTF-8 ub Terminal.app
 defaults write com.apple.terminal StringEncodings -array 4
@@ -11,6 +14,9 @@ defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 
 # show the ~/Library folder in Finder
 chflags nohidden ~/Library
+
+# wicked fast key repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 0
 
 # disable resume system wide
 # defaults write NSGlobalDomainNSQuitAlwaysKeepWindows -bool false
