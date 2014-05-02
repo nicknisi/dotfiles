4cb740f2a0e12aaeea6ad7384ca85e4a47eb7c6c
Nick Nisi <nick@nisi.org>
2014-05-02 16:09:10 -0500

    update keyboard repeat settings osx script

--- setup/osx.sh
+++ setup/osx.sh
@@ -90,10 +90,10 @@ echo "Disable press-and-hold for keys in favor of key repeat"
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 echo "Set a blazingly fast keyboard repeat rate"
-defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write NSGlobalDomain KeyRepeat -int 2
 
 echo "Set a shorter Delay until key repeat"
-defaults write NSGlobalDomain InitialKeyRepeat -int 12
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 #echo "Disable auto-correct"
 #defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
