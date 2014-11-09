c060dbe3bc3c6f319e43646931546e911ea9c83a
Nick Nisi <nick@nisi.org>
2014-11-09 16:49:54 -0600

    Optionally select theme for hl command

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -121,11 +121,17 @@ function gci() {
 
 # syntax highlight the contents of a file or the clipboard and place the result on the clipboard
 function hl() {
-    if [ -z "$2" ]; then
+    if [ -z "$3" ]; then
         src=$( pbpaste )
     else
-        src=$( cat $2 )
+        src=$( cat $3 )
     fi
 
-    echo $src | highlight -O rtf --syntax $1 --font Inconsoloata --style moria --font-size 24 | pbcopy
+    if [ -z "$2" ]; then
+        style="moria"
+    else
+        style="$2"
+    fi
+
+    echo $src | highlight -O rtf --syntax $1 --font Inconsoloata --style $style --line-number --font-size 24 | pbcopy
 }
