4677f0e526b136ca2313541d0c3be52e490588dd
Nick Nisi <nick@nisi.org>
2015-01-01 20:30:06 -0600

    turn on spotify tmux

--- tmux/theme.sh
+++ tmux/theme.sh
@@ -58,4 +58,4 @@ tm_host="#[fg=$tm_color_feature,bold]#h"
 tm_session_name="#[fg=$tm_color_feature,bold]$tm_icon #S"
 
 set -g status-left $tm_session_name
-set -g status-right $tm_itunes' '$tm_rdio' '$tm_date' '$tm_host
+set -g status-right $tm_itunes' '$tm_rdio' '$tm_spotify' '$tm_date' '$tm_host
