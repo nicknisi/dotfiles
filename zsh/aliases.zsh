1058ea646b89ee41b7325793b3bedfe869e25413
Nick Nisi <nick@nisi.org>
2015-08-24 18:34:36 -0500

    add chrome and canary aliases
    simplify the task of starting chrome and canary from the command line

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -71,3 +71,6 @@ alias pumpitup="osascript -e 'set volume 10'"
 # Kill all the tabs in Chrome to free up memory
 # [C] explained: http://www.commandlinefu.com/commands/view/402/exclude-grep-from-your-grepped-output-of-ps-alias-included-in-description
 alias chromekill="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs kill"
+
+alias chrome="/Applications/Google\\ \\Chrome.app/Contents/MacOS/Google\\ \\Chrome"
+alias canary="/Applications/Google\\ Chrome\\ Canary.app/Contents/MacOS/Google\\ Chrome\\ Canary"
