a61d3b67afdd206738e921224f1370f11aaedea3
Nick Nisi <nick@nisi.org>
2015-10-28 21:16:06 -0500

    fix symlink location in install script fixes #9
    Fix the symlink location in install/link.sh
    Thanks to @nilbot for pointing it out!

--- install/link.sh
+++ install/link.sh
@@ -7,5 +7,5 @@ linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
 for file in $linkables ; do
     target="$HOME/.$( basename $file ".symlink" )"
     echo "creating symlink for $file"
-    ln -s $DOTFILES/$file $target
+    ln -s $file $target
 done
