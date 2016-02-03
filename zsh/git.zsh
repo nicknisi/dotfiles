0583b0250f620214412a7be5c3ea2c0a5777e28f
Nick Nisi <nick@nisi.org>
2016-02-03 09:52:59 -0600

    clean up git log aliases
    * remove seldom used `git lg` alias
    * update colors/format of `git l` alias
    * update zsh alias `glog` to use `git l` instead of its own thing

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -11,7 +11,7 @@ alias gsp='git stash pop'
 alias gmv='git mv'
 alias grm='git rm'
 alias grn='git-rename'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glog="git l"
 
 # alias git-amend='git commit --amend -C HEAD'
 alias git-undo='git reset --soft HEAD~1'
