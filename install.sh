41cf057562c61bcf356ebeabd69b6d48f84cd31e
Nick Nisi <nick@nisi.org>
2017-01-12 06:06:48 -0800

    add better checking of zsh
    * add command_exists functio to check if a command exists
    * only chsh if shell isn't zsh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+command_exists() {
+    type "$1" > /dev/null 2>&1
+}
+
 echo "Installing dotfiles"
 
 echo "Initializing submodule(s)"
@@ -7,6 +11,7 @@ git submodule update --init --recursive
 
 source install/link.sh
 
+# only perform macOS-specific install
 if [ "$(uname)" == "Darwin" ]; then
     echo -e "\n\nRunning on OSX"
 
@@ -26,6 +31,13 @@ fi
 echo "creating vim directories"
 mkdir -p ~/.vim-tmp
 
+if ! command_exists zsh; then
+    echo "zsh not found. Please install and then re-run installation scripts"
+    exit 1
+elif ! [[ $SHELL =~ .*zsh.* ]]; then
+    echo "Configuring zsh as default shell"
+    chsh -s $(which zsh)
+fi
 
 echo "Configuring zsh as default shell"
 chsh -s $(which zsh)
