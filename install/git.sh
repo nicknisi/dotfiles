e42a108dd35562491fc031789625bec229a3271e
Nick Nisi <nick@nisi.org>
2017-04-03 16:44:05 -0500

    fix issues with git install script

--- install/git.sh
+++ install/git.sh
@@ -2,9 +2,9 @@
 
 printf "Setting up Git...\n\n"
 
-defaultName="Nick Nisi"
-defaultEmail="nick@nisi.org"
-defaultGithub="nicknisi"
+defaultName=$( git config --global user.name )
+defaultEmail=$( git config --global user.email )
+defaultGithub=$( git config --global github.user )
 
 read -p "Name [$defaultName] " name
 read -p "Email [$defaultEmail] " email
@@ -14,6 +14,6 @@ name=${name:-$defaultName}
 email=${email:-$defaultEmail}
 github=${github:-$defaultGithub}
 
-git config --global user.name $name
-git config --global user.meail $email
-git config --global github.user $github
+git config --global user.name "$name"
+git config --global user.meail "$email"
+git config --global github.user "$github"
