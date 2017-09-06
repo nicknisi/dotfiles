9bcdb371c1a9ba6daa94c17c47b7b3889c821ef4
Nick Nisi <nick@nisi.org>
2017-09-06 10:37:47 -0500

    disable Spotify tunes checking

--- applescripts/tunes.js
+++ applescripts/tunes.js
@@ -4,6 +4,11 @@ if (Application('iTunes').running()) {
     const artist = track.artist();
     const title = track.name();
     output = (`♫ ${title} - ${artist}`).substr(0, 50);
-}
+} /*else if (Application('Spotify').running()) {
+    const track = Application('Spotify').currentTrack;
+    const artist = track.artist();
+    const title = track.name();
+    output = (`♫ ${title} - ${artist}`).substr(0, 50);
+}*/
 
 output;
