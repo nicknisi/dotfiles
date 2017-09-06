658380bfede44c38f8374be38806f85a0fdd0bc3
Nick Nisi <nick@nisi.org>
2017-09-06 10:36:51 -0500

    remove upstream branch info in prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -76,4 +76,4 @@ precmd() {
 PROMPT_SYMBOL='❯'
 
 export PROMPT='%(?.%F{207}.%F{160})$PROMPT_SYMBOL%f '
-export RPROMPT='`git_dirty`%F{241}$vcs_info_msg_0_%f `upstream_branch``git_arrows``suspended_jobs`'
+export RPROMPT='`git_dirty`%F{241}$vcs_info_msg_0_%f`git_arrows``suspended_jobs`'
