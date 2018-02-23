5c0bd9460d8446108f8825d3a39d4e335fee1f1d
Nick Nisi <nick@nisi.org>
2018-02-23 13:56:05 -0600

    add various scripts, remove zsh functions
    - remove a few functions from being loaded each time
    - add colortest script
    - add confirm script
    - add extract script
    - add fh (search history) script
    - add git-kill script
    - add isdir script
    - add isfile script
    - add md script

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,23 +2,6 @@
 # functions
 ####################
 
-# print available colors and their numbers
-function colours() {
-    for i in {0..255}; do
-        printf "\x1b[38;5;${i}m colour${i}"
-        if (( $i % 5 == 0 )); then
-            printf "\n"
-        else
-            printf "\t"
-        fi
-    done
-}
-
-# Create a new directory and enter it
-function md() {
-    mkdir -p "$@" && cd "$@"
-}
-
 function ng-stop() {
     sudo launchctl stop homebrew.mxcl.nginx
 }
@@ -48,12 +31,6 @@ function server() {
     python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port"
 }
 
-# take this repo and copy it to somewhere else minus the .git stuff.
-function gitexport(){
-    mkdir -p "$1"
-    git archive master | tar -x -C "$1"
-}
-
 # get gzipped size
 function gz() {
     echo "orig size    (bytes): "
@@ -77,32 +54,4 @@ function escape() {
 function unidecode() {
     perl -e "binmode(STDOUT, ':utf8'); print \"$@\""
     echo # newline
-}
-
-# Extract archives - use: extract <file>
-# Credits to http://dotfiles.org/~pseup/.bashrc
-function extract() {
-    if [ -f $1 ] ; then
-        case $1 in
-            *.tar.bz2) tar xjf $1 ;;
-            *.tar.gz) tar xzf $1 ;;
-            *.bz2) bunzip2 $1 ;;
-            *.rar) rar x $1 ;;
-            *.gz) gunzip $1 ;;
-            *.tar) tar xf $1 ;;
-            *.tbz2) tar xjf $1 ;;
-            *.tgz) tar xzf $1 ;;
-            *.zip) unzip $1 ;;
-            *.Z) uncompress $1 ;;
-            *.7z) 7z x $1 ;;
-            *) echo "'$1' cannot be extracted via extract()" ;;
-        esac
-    else
-        echo "'$1' is not a valid file"
-    fi
-}
-
-# load zsh history up in FZF to search
-fh() {
-  print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
 }
