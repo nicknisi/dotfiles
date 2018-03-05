a55b8c822d37f926ca32538d03c0087840eeefe6
Nick Nisi <nick@nisi.org>
2018-03-05 07:54:33 -0600

    fix potential issues with link script fixes #74
    - replace for loop over files list with find command
    - clean up issues according to shellcheck
    
    Thanks to @tkothe for recommending a fix for this.

--- install/link.sh
+++ install/link.sh
@@ -2,33 +2,34 @@
 
 DOTFILES=$HOME/code/dotfiles
 
-echo -e "\nCreating symlinks"
+echo -e "\\nCreating symlinks"
 echo "=============================="
 linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
 for file in $linkables ; do
-    target="$HOME/.$( basename $file '.symlink' )"
-    if [ -e $target ]; then
+    target="$HOME/.$( basename "$file" '.symlink' )"
+    if [ -e "$target" ]; then
         echo "~${target#$HOME} already exists... Skipping."
     else
         echo "Creating symlink for $file"
-        ln -s $file $target
+        ln -s "$file" "$target"
     fi
 done
 
-echo -e "\n\ninstalling to ~/.config"
+echo -e "\\n\\ninstalling to ~/.config"
 echo "=============================="
-if [ ! -d $HOME/.config ]; then
+if [ ! -d "$HOME/.config" ]; then
     echo "Creating ~/.config"
-    mkdir -p $HOME/.config
+    mkdir -p "$HOME/.config"
 fi
 
-for config in $DOTFILES/config/*; do
-    target=$HOME/.config/$( basename $config )
-    if [ -e $target ]; then
+config_files=$( find "$DOTFILES/config" -d 1 2>/dev/null )
+for config in $config_files; do
+    target="$HOME/.config/$( basename "$config" )"
+    if [ -e "$target" ]; then
         echo "~${target#$HOME} already exists... Skipping."
     else
         echo "Creating symlink for $config"
-        ln -s $config $target
+        ln -s "$config" "$target"
     fi
 done
 
@@ -39,7 +40,7 @@ done
 # like to configure vim, so lets symlink ~/.vimrc and ~/.vim over to their
 # neovim equivalent.
 
-echo -e "\n\nCreating vim symlinks"
+echo -e "\\n\\nCreating vim symlinks"
 echo "=============================="
 VIMFILES=( "$HOME/.vim:$DOTFILES/config/nvim"
         "$HOME/.vimrc:$DOTFILES/config/nvim/init.vim" )
@@ -47,10 +48,10 @@ VIMFILES=( "$HOME/.vim:$DOTFILES/config/nvim"
 for file in "${VIMFILES[@]}"; do
     KEY=${file%%:*}
     VALUE=${file#*:}
-    if [ -e ${KEY} ]; then
+    if [ -e "${KEY}" ]; then
         echo "${KEY} already exists... skipping."
     else
         echo "Creating symlink for $KEY"
-        ln -s ${VALUE} ${KEY}
+        ln -s "${VALUE}" "${KEY}"
     fi
 done
