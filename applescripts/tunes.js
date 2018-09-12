d31aaa9094d833420c96a45f51ed6d82e1474c15
Nick Nisi <nick@nisi.org>
2018-09-12 10:31:56 -0500

    uncomment spotify

--- applescripts/tunes.js
+++ applescripts/tunes.js
@@ -4,11 +4,11 @@ if (Application('iTunes').running()) {
     const artist = track.artist();
     const title = track.name();
     output = (`${title} - ${artist}`).substr(0, 50);
-} /*else if (Application('Spotify').running()) {
+} else if (Application('Spotify').running()) {
     const track = Application('Spotify').currentTrack;
     const artist = track.artist();
     const title = track.name();
-    output = (`♫ ${title} - ${artist}`).substr(0, 50);
-}*/
+    output = (`${title} - ${artist}`).substr(0, 50);
+}
 
 output;
