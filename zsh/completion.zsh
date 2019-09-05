8bc6cd493df9211c4009919a450c0fad96cd570d
Nick Nisi <nick@nisi.org>
2019-09-04 22:12:02 -0500

    consolidate zsh scripts into zshrc and functions
    - add git completions to fpath in zshenv
    - remove unused (and therefore, unmaintained) functions, aliases, etc.
    - consolidate aliases and configuration into zshrc
    - move remamining functions into functions/

--- zsh/completion.zsh
+++
@@ -1,8 +0,0 @@
-# matches case insensitive for lowercase
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-
-# pasting with tabs doesn't perform completion
-zstyle ':completion:*' insert-tab pending
-
-# default to file completion
-zstyle ':completion:*' completer _expand _complete _files _correct _approximate
--- zsh/git_completion.sh
+++
@@ -1,6 +0,0 @@
-# this is specific to the location of the current version of git, installed by homebrew
-completion=/usr/local/Cellar/git/2.20.1/etc/bash_completion.d/git-completion.zsh
-
-if test -f $completion; then
-    source $completion
-fi
