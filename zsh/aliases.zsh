9c545e1e1fe67cf95751666ead5b40266e4ed6fb
Nick Nisi <nick@nisi.org>
2019-09-04 20:58:34 -0500

    update ios alias to open simulator

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -29,7 +29,7 @@ alias df='df -h' # disk free, in Gigabytes, not bytes
 alias du='du -h -c' # calculate disk usage for a folder
 
 # Applications
-alias ios='open -a /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone\ Simulator.app'
+alias ios='open -a /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app'
 
 # IP addresses
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
