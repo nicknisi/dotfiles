9a891f973cf5a2af46f363532e241ae27c9d2efd
Nick Nisi <nick@nisi.org>
2020-02-26 09:47:36 -0600

    macOS set faster key repeat

--- install/osx.sh
+++ install/osx.sh
@@ -93,7 +93,7 @@ echo "Disable press-and-hold for keys in favor of key repeat"
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 echo "Set a blazingly fast keyboard repeat rate"
-defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain KeyRepeat -int 1
 
 echo "Set a shorter Delay until key repeat"
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
