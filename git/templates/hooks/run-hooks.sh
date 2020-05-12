362f89a3d39f0b0c4abe33c182e22c4d3a917702
Nick Nisi <nick@nisi.org>
2020-05-12 15:47:47 -0500

    convert run-hooks to bash script fixes #138
    Run the script as a bash script to ensure shopt exists in most
    environments and fix shellcheck warnings.

--- git/templates/hooks/run-hooks.sh
+++ git/templates/hooks/run-hooks.sh
@@ -1,22 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 EXIT_CODE=0
 
-repo=$( git rev-parse --show-toplevel )
-hook_type=$( basename $0 )
+hook_type=$( basename "$0" )
 hooks=~/.dotfiles/git/hooks
 
 shopt -s nullglob
-for hook in $hooks/*.$hook_type; do
+for hook in "$hooks"/*."$hook_type"; do
 	echo ""
 	echo "${COLOR_LIGHTPURPLE}Executing ${hook}${COLOR_NONE}"
 	${hook}
-	EXIT_CODE=$((${EXIT_CODE} + $?))
+	EXIT_CODE=$((EXIT_CODE + $?))
 done
 
 if [[ ${EXIT_CODE} -ne 0 ]]; then
 	echo ""
-	echo "${COLOR_RED}Commit Failed.${COLOR_NONE}"
+	echo "${COLOR_RED}$hook_type hook failed.${COLOR_NONE}"
 fi
 
-exit $((${EXIT_CODE}))
+exit $((EXIT_CODE))
