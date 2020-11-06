dba492b164274a8535bd43ef5a98b83b9277088d
Nick Nisi <nick@nisi.org>
2020-11-06 13:37:43 -0600

    feat: initial commit of checkhealth script

---
+++ checkhealth.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+# Check the health of the dotfiles installation.
+
+# Things to check:
+# - [ ] check symlinks
+# - [ ] check XDG_CONFIG
+# - [ ] check key utilies are installed
+# - [ ] check shell
+# - [ ] check the path
+
+COLOR_GRAY="\033[1;38;5;243m"
+COLOR_BLUE="\033[1;34m"
+COLOR_GREEN="\033[1;32m"
+COLOR_RED="\033[1;31m"
+COLOR_PURPLE="\033[1;35m"
+COLOR_YELLOW="\033[1;33m"
+COLOR_NONE="\033[0m"
+newline=$'\n'
+
+title() {
+    echo -e "\n${COLOR_PURPLE}$1${COLOR_NONE}"
+    echo -e "${COLOR_GRAY}==============================${COLOR_NONE}\n"
+}
+
+error() {
+    echo -e "${COLOR_RED}Error: ${COLOR_NONE}$1"
+    exit 1
+}
+
+warning() {
+    printf "${COLOR_GREEN}" "$*" "${COLOR_NONE}"
+}
+
+success() {
+    printf "${COLOR_GREEN}" "$*" "${COLOR_NONE}"
+}
+
+get_linkables() {
+    find -H "$DOTFILES" -maxdepth 3 -name '*.symlink'
+}
+
+check_utils() {
+    local utils=(git brew tmux nvim kitty)
+    title "Checking for installed apps"
+    for util in "${utils[@]}"; do
+        printf %s "Checking $util... "
+
+        if test "$(command -v "$util")"; then
+            success "Exists."
+        else
+            warning "MISSING. "
+        fi
+        printf %s "$newline"
+    done
+}
+
+check_symlinks() {
+    title "Checking Symlinks"
+
+    for link in $(get_linkables) ; do
+        target="$HOME/.$(basename "$link" '.symlink')"
+        printf %s "Checking Symlink for \"$target\"... "
+        if [[ -h "$target" ]]; then
+            if [ "$link" != "$(readlink "$target")" ]; then
+                warn "$target is not properly symlinked"
+            else
+                success "Exists."
+            fi
+        else
+            warn "$target symlink is missing."
+        fi
+        printf %s "$newline"
+    done
+}
+
+# title "Dotfiles health check"
+
+# check_utils
+check_symlinks
