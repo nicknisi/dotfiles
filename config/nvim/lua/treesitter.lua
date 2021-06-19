f872a3243b112d438b0beec4beff105c97f9b2cd
Nick Nisi <nick@nisi.org>
2021-06-19 06:57:57 -0500

    feat(vim): enable and configure treesitter/base16

---
+++ config/nvim/lua/treesitter.lua
@@ -0,0 +1,9 @@
+local ts = require('nvim-treesitter.configs')
+
+ts.setup({
+    ensure_installed = 'maintained',
+    highlight = {
+        enable = true,
+        use_languagetree = true,
+    }
+})
