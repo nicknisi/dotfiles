8b396af78343639755daace9930c3e38535666ca
Nick Nisi <nick@nisi.org>
2021-06-20 14:54:54 -0500

    fix(vim): keymap for file tree finds file
    Set up script to call NvimTreeFindFile or close

--- config/nvim/lua/nvimtree.lua
+++ config/nvim/lua/nvimtree.lua
@@ -1,11 +1,22 @@
+local view = require('nvim-tree.view')
+_G.NvimTreeConfig = {}
+
+function NvimTreeConfig.find_toggle()
+    if view.win_open() then
+        view.close()
+    else
+        vim.cmd('NvimTreeFindFile')
+    end
+end
+
 vim.api.nvim_set_keymap(
     'n',
     '<leader>k',
-    ':NvimTreeToggle<CR>',
+    '<CMD>lua NvimTreeConfig.find_toggle()<CR>',
     {
         noremap = true,
         silent = true,
     }
 )
 
-require('nvim-tree.view').View.width = 40
+view.width = 40
