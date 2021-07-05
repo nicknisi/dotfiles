a781605a5c6b78088199649382e63cf27f95cc2d
ajr-dev <arturojosejr@gmail.com>
2021-07-05 23:02:42 +0200

    fix brew installation on linux

--- install.sh
+++ install.sh
@@ -155,9 +155,7 @@ setup_homebrew() {
     fi
 
     if [ "$(uname)" == "Linux" ]; then
-        test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
         test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-        test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
     fi
 
     # install brew dependencies from Brewfile
