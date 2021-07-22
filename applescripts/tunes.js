0d6210d96519e9acaf47c29be9f846c28eeb09d2
Nick Nisi <nick@nisi.org>
2021-07-22 15:03:17 -0500

    chore(tmux): remove iTunes references from tunes
    - remove iTunes from tunes JXA
    - remove the tunes AppleScript as it's no longer maintained

--- applescripts/tunes.js
+++ applescripts/tunes.js
@@ -4,11 +4,6 @@ if (Application("Music").running()) {
     const artist = track.artist();
     const title = track.name();
     output = `${title} - ${artist}`.substr(0, 50);
-} else if (Application("iTunes").running()) {
-    const track = Application("iTunes").currentTrack;
-    const artist = track.artist();
-    const title = track.name();
-    output = `${title} - ${artist}`.substr(0, 50);
 } else if (Application("Spotify").running()) {
     const track = Application("Spotify").currentTrack;
     const artist = track.artist();
