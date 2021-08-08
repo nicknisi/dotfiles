41644b1fe493c04605ec318b127411be391d90e7
Nick Nisi <nick@nisi.org>
2021-08-07 23:14:51 -0500

    fix(vim): remove deprecated gitsigns setting

--- config/nvim/lua/settings/gitsigns.lua
+++ config/nvim/lua/settings/gitsigns.lua
@@ -36,6 +36,5 @@ require("gitsigns").setup {
   update_debounce = 100,
   status_formatter = nil, -- Use default
   word_diff = true,
-  use_decoration_api = true,
   use_internal_diff = true -- If luajit is present
 }
