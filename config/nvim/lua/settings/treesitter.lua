81230a784161c2efd4b21bad96ecf4cfca12ec6d
Nick Nisi <nick@nisi.org>
2021-08-09 22:46:59 -0500

    feat(vim): set up treesitter and plugins

--- config/nvim/lua/settings/treesitter.lua
+++ config/nvim/lua/settings/treesitter.lua
@@ -1,11 +1,24 @@
-local ts = require("nvim-treesitter.configs")
-
-ts.setup(
-  {
-    ensure_installed = "maintained",
-    highlight = {
-      enable = true,
-      use_languagetree = true
+require("nvim-treesitter.configs").setup {
+  ensure_installed = "maintained",
+  highlight = {
+    enable = true,
+    use_languagetree = true
+  },
+  indent = {enable = true},
+  content_commentstring = {enable = true},
+  rainbow = {
+    enable = false,
+    extended_mode = true,
+    max_file_lines = 1000
+  },
+  autotag = {
+    enable = true,
+    filetypes = {
+      "html",
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact"
     }
   }
-)
+}
