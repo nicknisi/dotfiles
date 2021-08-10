b8215fcb916b88c158a910b153108ff020ae6240
Nick Nisi <nick@nisi.org>
2021-08-10 07:24:14 -0500

    feat(vim): add treesitter textobjects plugin

--- config/nvim/lua/settings/treesitter.lua
+++ config/nvim/lua/settings/treesitter.lua
@@ -5,11 +5,31 @@ require("nvim-treesitter.configs").setup {
     use_languagetree = true
   },
   indent = {enable = true},
-  content_commentstring = {enable = true},
   rainbow = {
     enable = false,
     extended_mode = true,
     max_file_lines = 1000
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true, -- automatically jump forward to matching textobj
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner"
+      }
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<leader>a"] = "@parameter.inner"
+      },
+      swap_previous = {
+        ["<leader>A"] = "@parameter.inner"
+      }
+    }
   },
   playground = {
     enable = true,
