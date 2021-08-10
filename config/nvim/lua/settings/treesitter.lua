53a6ab91f18d7330254f0474bfe5cf7eb5aa960b
Nick Nisi <nick@nisi.org>
2021-08-09 23:09:17 -0500

    feat(vim): add ts-playground plugin

--- config/nvim/lua/settings/treesitter.lua
+++ config/nvim/lua/settings/treesitter.lua
@@ -11,14 +11,22 @@ require("nvim-treesitter.configs").setup {
     extended_mode = true,
     max_file_lines = 1000
   },
-  autotag = {
+  playground = {
     enable = true,
-    filetypes = {
-      "html",
-      "javascript",
-      "javascriptreact",
-      "typescript",
-      "typescriptreact"
+    disable = {},
+    updatetime = 25,
+    persist_queries = false,
+    keybindings = {
+      toggle_query_editor = "o",
+      toggle_hl_groups = "i",
+      toggle_injected_languages = "t",
+      toggle_anonymous_nodes = "a",
+      toggle_language_display = "I",
+      focus_language = "f",
+      unfocus_language = "F",
+      update = "R",
+      goto_node = "<cr>",
+      show_help = "?"
     }
   }
 }
