76527d7a307850493660918595d186ef68ac9a3a
Nick Nisi <nick@nisi.org>
2021-08-16 21:59:20 -0500

    feat(vim): update LSP diagnostic error symbol to 

--- config/nvim/lua/settings/lspconfig.lua
+++ config/nvim/lua/settings/lspconfig.lua
@@ -260,7 +260,7 @@ lspinstall.post_install_hook = function()
 end
 
 -- set up custom symbols for LSP errors
-fn.sign_define("LspDiagnosticsSignError", {text = "✖", texthl = "LspDiagnosticsSignError", linehl = "", numhl = ""})
+fn.sign_define("LspDiagnosticsSignError", {text = "", texthl = "LspDiagnosticsSignError", linehl = "", numhl = ""})
 fn.sign_define("LspDiagnosticsSignWarning", {text = "⚠", texthl = "LspDiagnosticsSignWarning"})
 fn.sign_define("LspDiagnosticsSignInformation", {text = "●", texthl = "LspDiagnosticsSignInfo"})
 fn.sign_define("LspDiagnosticsSignHint", {text = "○", texthl = "LspDiagnosticsSignHint"})
