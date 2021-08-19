aff29eec7692536638274c56fc17ec05c75e86e8
Nick Nisi <nick@nisi.org>
2021-08-19 14:26:57 -0500

    feat(vim): customize nvim-tree icons and follow

--- config/nvim/lua/settings/nvimtree.lua
+++ config/nvim/lua/settings/nvimtree.lua
@@ -3,6 +3,38 @@ local nnoremap = require("utils").nnoremap
 local view = require("nvim-tree.view")
 _G.NvimTreeConfig = {}
 
+vim.g.nvim_tree_follow = 1
+
+vim.g.nvim_tree_icons = {
+  default = "",
+  symlink = "",
+  git = {
+    unstaged = "●",
+    staged = "✓",
+    unmerged = "",
+    renamed = "➜",
+    untracked = "★",
+    deleted = "",
+    ignored = "◌"
+  },
+  folder = {
+    arrow_open = "",
+    arrow_closed = "",
+    default = "",
+    open = "",
+    empty = "",
+    empty_open = "",
+    symlink = "",
+    symlink_open = ""
+  },
+  lsp = {
+    hint = "",
+    info = "",
+    warning = "",
+    error = ""
+  }
+}
+
 function NvimTreeConfig.find_toggle()
   if view.win_open() then
     view.close()
