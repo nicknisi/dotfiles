3584cee05388ad2a1fac94caa52f68a1dac8b4ed
Nick Nisi <nick@nisi.org>
2021-08-26 21:21:07 -0500

    refactor(vim): create colors file, use to style statusline

--- config/nvim/lua/settings/galaxyline.lua
+++ config/nvim/lua/settings/galaxyline.lua
@@ -1,5 +1,6 @@
 local gl = require("galaxyline")
 local gls = gl.section
+local colors = require("settings.colors")
 
 -- local colors = require("tokyonight.colors").setup()
 local condition = require("galaxyline.condition")
@@ -8,32 +9,6 @@ local lsp = require("galaxyline.provider_lsp")
 local vcs = require("galaxyline.provider_vcs")
 
 gl.short_line_list = {"NvimTree", "help", "tagbar"}
-
-local colors = {
-  bg = "#202328",
-  fg = "#bbc2cf",
-  aqua = "#3affdb",
-  beige = "#f5c06f",
-  blue = "#51afef",
-  brown = "#905532",
-  cyan = "#008080",
-  darkblue = "#081633",
-  darkorange = "#f16529",
-  green = "#98be65",
-  grey = "#8c979a",
-  lightblue = "#5fd7ff",
-  lightgreen = "#31b53e",
-  magenta = "#c678dd",
-  orange = "#d4843e",
-  pink = "#cb6f6f",
-  purple = "#834f79",
-  red = "#ae403f",
-  salmon = "#ee6e73",
-  violet = "#a9a1e1",
-  white = "#eff0f1",
-  yellow = "#f09f17",
-  black = "#202328"
-}
 
 -- Maps
 local mode_color = {
