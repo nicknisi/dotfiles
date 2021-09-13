c99ba7bc3ba5c63922105a7c5dc0fb948bc3e29a
Nick Nisi <nick@nisi.org>
2021-09-12 22:12:56 -0500

    fix(vim): replace deprecated gitsigns setting
    use_internal_diff -> diff_opts.internal

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -38,5 +38,7 @@ require("gitsigns").setup {
   update_debounce = 100,
   status_formatter = nil, -- Use default
   word_diff = true,
-  use_internal_diff = true -- If luajit is present
+  diff_opts = {
+    internal = true
+  }
 }
