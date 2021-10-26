a60da7eb7d73c815a38431d7f183b22dc6cfb20f
Nick Nisi <nick@nisi.org>
2021-10-25 21:35:19 -0500

    fix(vim): completion selection with nvim-cmp

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -11,8 +11,8 @@ cmp.setup(
       end
     },
     mapping = {
-      ["<C-j>"] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select}),
-      ["<C-k>"] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select}),
+      ["<C-j>"] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Insert}),
+      ["<C-k>"] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Insert}),
       ["<C-d>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),
