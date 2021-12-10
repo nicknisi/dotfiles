48d9f0f00cde09854203d38530766e2fca462db2
Nick Nisi <nick@nisi.org>
2021-12-10 13:24:54 -0600

    fix(vim): update trouble binding to deprecated call

--- config/nvim/lua/plugins/trouble.lua
+++ config/nvim/lua/plugins/trouble.lua
@@ -3,8 +3,8 @@ local nnoremap = require("utils").nnoremap
 
 -- trouble keymappings
 nnoremap("<leader>xx", "<cmd>TroubleToggle<cr>")
-nnoremap("<leader>xw", "<cmd>TroubleToggle lsp_workspace_diagnostics<cr>")
-nnoremap("<leader>xd", "<cmd>TroubleToggle lsp_document_diagnostics<cr>")
+nnoremap("<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>")
+nnoremap("<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>")
 nnoremap("<leader>xq", "<cmd>TroubleToggle quickfix<cr>")
 nnoremap("<leader>xl", "<cmd>TroubleToggle loclist<cr>")
 -- nnoremap("gR", "<cmd>TroubleToggle lsp_references<cr>")
