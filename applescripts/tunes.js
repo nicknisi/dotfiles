9b1f9ef77b44987fda073c70158171eca75c183e
Nick Nisi <nick@nisi.org>
2022-01-24 14:20:07 -0600

    feat(tmux): show music in all tmux themes

--- applescripts/tunes.js
+++ applescripts/tunes.js
@@ -1,15 +1,14 @@
-const symbol = "";
 let output = "";
 if (Application("Music").running()) {
     const track = Application("Music").currentTrack;
     const artist = track.artist();
     const title = track.name();
-    output = `${symbol} ${title} - ${artist}`.substr(0, 50);
+    output = `${title} - ${artist}`.substr(0, 50);
 } else if (Application("Spotify").running()) {
     const track = Application("Spotify").currentTrack;
     const artist = track.artist();
     const title = track.name();
-    output = `${symbol} ${title} - ${artist}`.substr(0, 50);
+    output = `${title} - ${artist}`.substr(0, 50);
 }
 
 output;
