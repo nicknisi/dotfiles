90fd0fabe9e3fd0212df1fa10a1b1895b20ecdc5
Nick Nisi <nick@nisi.org>
2022-02-02 06:51:56 -0600

    feat(vim): detect local prettier and use it
    - use local version of prettier, if available
    - consolidate prettier config into a reusable function

--- config/nvim/lua/plugins/formatter.lua
+++ config/nvim/lua/plugins/formatter.lua
@@ -1,36 +1,35 @@
+-- on startup, check if there exists a local version of prettier
+-- in the project and use that. Otherwise, use the global version.
+local prettier_path = "./node_modules/.bin/prettier"
+if vim.fn.executable(prettier_path) ~= 1 then
+  prettier_path = "prettier"
+end
+
+function prettier_config()
+  return {
+    exe = prettier_path,
+    args = {
+      "--config-precedence",
+      "prefer-file",
+      "--stdin-filepath",
+      vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
+    },
+    stdin = true
+  }
+end
+
 require("formatter").setup(
   {
     logging = false,
     filetype = {
       javascript = {
-        -- prettier
-        function()
-          return {
-            exe = "prettier",
-            args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0)},
-            stdin = true
-          }
-        end
+        prettier_config
       },
       typescript = {
-        -- prettier
-        function()
-          return {
-            exe = "prettier",
-            args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0)},
-            stdin = true
-          }
-        end
+        prettier_config
       },
       ["typescript.tsx"] = {
-        -- prettier
-        function()
-          return {
-            exe = "prettier",
-            args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0)},
-            stdin = true
-          }
-        end
+        prettier_config
       },
       lua = {
         -- luafmt
