8d076db35ade50b1e91107a8d4b5ecfe6344e1fd
Nick Nisi <nick@nisi.org>
2022-05-30 13:19:04 -0500

    fix(vim): fix deprecated warnings on nvim_tree

--- config/nvim/lua/plugins/nvimtree.lua
+++ config/nvim/lua/plugins/nvimtree.lua
@@ -5,36 +5,6 @@ local icons = require("theme").icons
 local view = require("nvim-tree.view")
 _G.NvimTreeConfig = {}
 
-vim.g.nvim_tree_icons = {
-  default = icons.file,
-  symlink = icons.symlink,
-  git = {
-    unstaged = icons.unmerged,
-    staged = icons.staged,
-    unmerged = icons.unmerged,
-    renamed = icons.renamed,
-    untracked = icons.untracked,
-    deleted = icons.deleted,
-    ignored = icons.ignored
-  },
-  folder = {
-    arrow_open = icons.arrow_open,
-    arrow_closed = icons.arrow_closed,
-    default = icons.default,
-    open = icons.open,
-    empty = icons.empty,
-    empty_open = icons.empty_open,
-    symlink = icons.symlink,
-    symlink_open = icons.symlink_open
-  },
-  lsp = {
-    hint = icons.hint,
-    info = icons.info,
-    warning = icons.warning,
-    error = icons.error
-  }
-}
-
 function NvimTreeConfig.find_toggle()
   if view.is_visible() then
     view.close()
@@ -49,7 +19,47 @@ nvimtree.setup {
   disable_netrw = false,
   hijack_netrw = true,
   diagnostics = {
-    enable = false
+    enable = false,
+    icons = {
+      hint = icons.hint,
+      info = icons.info,
+      warning = icons.warning,
+      error = icons.error
+    }
+  },
+  renderer = {
+    add_trailing = false,
+    group_empty = true,
+    highlight_git = true,
+    highlight_opened_files = "all",
+    indent_markers = {
+      enable = true
+    },
+    icons = {
+      glyphs = {
+        default = icons.file,
+        symlink = icons.symlink,
+        git = {
+          unstaged = icons.unmerged,
+          staged = icons.staged,
+          unmerged = icons.unmerged,
+          renamed = icons.renamed,
+          untracked = icons.untracked,
+          deleted = icons.deleted,
+          ignored = icons.ignored
+        },
+        folder = {
+          arrow_open = icons.arrow_open,
+          arrow_closed = icons.arrow_closed,
+          default = icons.default,
+          open = icons.open,
+          empty = icons.empty,
+          empty_open = icons.empty_open,
+          symlink = icons.symlink,
+          symlink_open = icons.symlink_open
+        }
+      }
+    }
   },
   update_focused_file = {
     enable = true,
