463374723698fc0b713d28a538a275e7118cf05e
Nick Nisi <nick@nisi.org>
2022-12-31 08:06:59 -0600

    feat(zsh): autoload .zsh files, split aliases
    - Check for all checked-in '*.zsh' files and source them
    - Separate out aliases for each of the apps (git, nvim, etc.)
    - Clean up unused things in zshrc
    - Remove utils.zsh and add the fuctions directly to the prompt

---
+++ zsh/aliases.zsh
@@ -0,0 +1,33 @@
+# reload zsh config
+alias reload!='RELOAD=1 source ~/.zshrc'
+
+# Filesystem aliases
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+alias l="ls -lah ${colorflag}"
+alias la="ls -AF ${colorflag}"
+alias ll="ls -lFh ${colorflag}"
+alias lld="ls -l | grep ^d"
+alias rmf="rm -rf"
+
+# Helpers
+alias grep='grep --color=auto'
+alias df='df -h' # disk free, in Gigabytes, not bytes
+alias du='du -h -c' # calculate disk usage for a folder
+
+alias lpath='echo $PATH | tr ":" "\n"' # list the PATH separated by new lines
+
+# Applications
+alias ios='open -a /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app'
+
+# Hide/show all desktop icons (useful when presenting)
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
+# Recursively delete `.DS_Store` files
+alias cleanup="find . -name '*.DS_Store' -type f -ls -delete"
+# remove broken symlinks
+alias clsym="find -L . -name . -o -type d -prune -o -type l -exec rm {} +"
