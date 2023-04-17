c4a9d8e47f99262eb0cc8508a9bfff8f7fbfc31d
Nick Nisi <nicknisi@KWRVXF346X.local.tld>
2023-04-17 08:36:20 -0500

    fix(vim): mason config for nvim 0.9

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -13,12 +13,14 @@ local null_ls = require("null-ls")
 
 mason.setup({ ui = { border = border } })
 
-mason_null_ls.setup({ automatic_setup = true, ensure_installed = { "stylua", "prettier" } })
-
-mason_null_ls.setup_handlers({
-  function(source_name, methods)
-    require("mason-null-ls.automatic_setup")(source_name, methods)
-  end,
+mason_null_ls.setup({
+  automatic_setup = true,
+  ensure_installed = { "stylua", "prettier" },
+  handlers = {
+    function(source_name, methods)
+      require("mason-null-ls.automatic_setup")(source_name, methods)
+    end,
+  }
 })
 
 null_ls.setup({
