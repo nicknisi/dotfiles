6fb2222bb44ac5669ab69d0b436cecb2dc10c3e8
Nick Nisi <nick@nisi.org>
2023-04-23 08:52:01 -0500

    refactor: more experimentation with wez colors

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local config = {
   color_scheme = "Catppuccin Mocha",
-  window_background_opacity = 0.9,
+  window_background_opacity = 0.85,
   enable_tab_bar = false,
   window_decorations = "RESIZE",
   font = wezterm.font("MonoLisa", { weight = "Medium" }),
@@ -17,11 +17,16 @@ local config = {
   window_background_gradient = {
     orientation = "Horizontal",
     colors = {
-      "#0f0c29",
+      -- "#0f0c29",
       -- "#1e1e2d",
-      "#302b63",
+      -- "#302b63",
+      "#00000C",
+      -- "#00003F",
+      "#000026",
+      "#00000C",
       -- "#24243e",
-      "#1e1e2d",
+      -- "#1e1e2d",
+      -- "#0f0c29",
     },
     interpolation = "CatmullRom",
     blend = "Rgb",
