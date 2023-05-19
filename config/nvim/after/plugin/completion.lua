46ee82f5c66ae39c66029e4225ff665d8782279a
Nick Nisi <nick@nisi.org>
2023-05-19 09:46:19 -0500

    fix(vim): fix issue with completion and virtual text

--- config/nvim/after/plugin/completion.lua
+++ config/nvim/after/plugin/completion.lua
@@ -5,6 +5,7 @@ local tailwind_formatter = require("tailwindcss-colorizer-cmp").formatter
 vim.o.completeopt = "menu,menuone,noselect"
 
 cmp.setup({
+  ghost_text = { enabled = true },
   snippet = {
     expand = function(args)
       vim.fn["vsnip#anonymous"](args.body)
@@ -13,7 +14,7 @@ cmp.setup({
   mapping = {
     ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
     ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-    ["<C-d>"] = cmp.mapping.scroll_docs( -4),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
@@ -23,7 +24,7 @@ cmp.setup({
     { name = "vsnip" },
     { name = "nvim_lua" },
     { name = "nvim_lsp" },
-    { name = "buffer",  keyword_length = 5, max_item_count = 5 },
+    { name = "buffer", keyword_length = 5, max_item_count = 5 },
     { name = "path" },
   }),
   formatting = {
@@ -43,5 +44,5 @@ cmp.setup({
       before = tailwind_formatter,
     }),
   },
-  experimental = { native_menu = false, ghost_text = true },
+  experimental = { native_menu = false, ghost_text = { enabled = true } },
 })
