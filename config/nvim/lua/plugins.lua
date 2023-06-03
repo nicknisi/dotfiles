827f933e19ced2cbd75c17bba680b6ff5644dee2
Nick Nisi <nick@nisi.org>
2023-06-03 08:20:53 -0500

    feat(vim): add nvim-notify plugin and configure it

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -192,6 +192,33 @@ require("lazy").setup({
   { "windwp/nvim-autopairs", config = true },
   -- Style the tabline without taking over how tabs and buffers work in Neovim
   { "alvarosevilla95/luatab.nvim", config = true }, -- enable copilot support for Neovim
+  { "MunifTanjim/nui.nvim", lazy = true },
+  {
+    "rcarriga/nvim-notify",
+    keys = {
+      {
+        "<leader>un",
+        function()
+          require("notify").dismiss({ silent = true, pending = true })
+        end,
+        desc = "Dismiss all Notifications",
+      },
+    },
+    opts = {
+      timeout = 3000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
+      background_colour = "#1e222a",
+      render = "minimal",
+    },
+    init = function()
+      vim.notify = require("notify")
+    end,
+  },
   {
     "github/copilot.vim",
     -- uncomment to test the lua rewrite
