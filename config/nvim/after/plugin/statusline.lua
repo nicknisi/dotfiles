74941c81f3ac2c82b0d09be7952dce16b0cfcb76
Nick Nisi <nick@nisi.org>
2023-07-11 11:53:04 -0500

    feat(nvim): add macro recording status to lualine

--- config/nvim/after/plugin/statusline.lua
+++ config/nvim/after/plugin/statusline.lua
@@ -1,4 +1,15 @@
-require("lualine").setup({
+local lualine = require("lualine")
+
+local function show_macro_recording()
+  local recording_register = vim.fn.reg_recording()
+  if recording_register == "" then
+    return ""
+  else
+    return "Recording @" .. recording_register
+  end
+end
+
+lualine.setup({
   options = {
     icons_enabled = true,
     theme = "catppuccin",
@@ -6,7 +17,7 @@ require("lualine").setup({
     component_separators = "", --{ right = "", left = "" },
   },
   sections = {
-    lualine_a = { "mode" },
+    lualine_a = { { "mode" }, { "macro-recording", fmt = show_macro_recording } },
     lualine_b = {
       "branch",
       { "diff", symbols = { added = " ", modified = " ", removed = "󰮉 " } },
@@ -17,4 +28,11 @@ require("lualine").setup({
     lualine_y = { "progress" },
     lualine_z = { "location" },
   },
+})
+
+vim.api.nvim_create_autocmd("RecordingEnter", {
+  callback = function()
+    -- refresh lualine when entering record mode
+    lualine.refresh({ place = { "lualine_a" } })
+  end,
 })
