f2c53a525b5ddf7f8cf9b0a641e590a4c44ed4dc
Nick Nisi <nick@nisi.org>
2023-07-28 21:35:28 -0500

    fix: wezterm send option key through
    this fixes issue in nvim where opt-j/k wasn't working to move line up or down a line

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -17,6 +17,8 @@ local config = {
       action = wezterm.action.ToggleFullScreen,
     },
   },
+  send_composed_key_when_left_alt_is_pressed = true,
+  send_composed_key_when_right_alt_is_pressed = false,
 }
 
 local appearance = wezterm.gui.get_appearance()
