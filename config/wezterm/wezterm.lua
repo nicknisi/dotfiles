514e7cf6cd7c021439601d3dd609050df266782b
Nick Nisi <nick@nisi.org>
2023-08-02 17:52:44 -0500

    style: update wezterm styles

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -50,32 +50,51 @@ if appearance:find("Dark") then
       height = "100%",
       opacity = 0.75,
     },
+    -- {
+    --   source = {
+    --     File = {
+    --       path = wezdir .. "/lines.gif",
+    --       speed = 0.40,
+    --     },
+    --   },
+    --   repeat_y = "Mirror",
+    --   width = "100%",
+    --   -- height = "100%",
+    --   opacity = 0.25,
+    --   hsb = {
+    --     hue = 0.2,
+    --     saturation = 0.2,
+    --     brightness = 0.01,
+    --   },
+    -- },
     {
       source = {
-        File = { path = wezdir .. "/pulsing.gif", speed = 0.4 },
+        File = { path = wezdir .. "/blob_blue.gif", speed = 0.3 },
       },
-      repeat_y = "Mirror",
-      width = "100%",
-      opacity = 0.05,
+      repeat_x = "Mirror",
+      -- width = "100%",
+      height = "100%",
+      opacity = 0.10,
       hsb = {
-        hue = 0.6,
+        hue = 0.9,
         saturation = 0.9,
-        brightness = 0.1,
+        brightness = 0.3,
       },
     },
-    {
-      source = {
-        File = { path = wezdir .. "/4.gif", speed = 0.3 },
-      },
-      repeat_y = "Mirror",
-      width = "100%",
-      opacity = 0.20,
-      hsb = {
-        hue = 0.6,
-        saturation = 0.9,
-        brightness = 0.1,
-      },
-    },
+    -- {
+    --   source = {
+    --     File = { path = wezdir .. "/blob.gif", speed = 0.3 },
+    --   },
+    --   repeat_x = "Mirror",
+    --   -- width = "100%",
+    --   height = "100%",
+    --   opacity = 0.20,
+    --   hsb = {
+    --     hue = 0.5,
+    --     saturation = 0.9,
+    --     brightness = 0.2,
+    --   },
+    -- },
   }
 else
   config.color_scheme = "Catppuccin Latte"
