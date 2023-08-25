84d36a6108afeaff2258cf06c82ab2eec9dc55aa
Nick Nisi <nick@nisi.org>
2023-08-24 21:09:07 -0500

    feat(vim): add boole.nvim plugin to toggle booleans, etc.

--- config/nvim/lua/plugins/core.lua
+++ config/nvim/lua/plugins/core.lua
@@ -57,4 +57,24 @@ return {
       { "<leader>ss", "<cmd>lua require('spectre').open()<cr>", desc = "open spectre" },
     },
   },
+  {
+    "nat-418/boole.nvim",
+    config = {
+      mappings = {
+        increment = "<C-a>",
+        decrement = "<C-x>",
+      },
+      -- User defined loops
+      additions = {
+        -- { "Foo", "Bar" },
+        -- { "tic", "tac", "toe" },
+      },
+      allow_caps_additions = {
+        { "enable", "disable" },
+        -- enable → disable
+        -- Enable → Disable
+        -- ENABLE → DISABLE
+      },
+    },
+  },
 }
