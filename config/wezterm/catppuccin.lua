9102d95fcb9757ae869e32e79cf7da767e985e18
Nick Nisi <nick@nisi.org>
2023-12-07 11:50:46 -0600

    style(wezterm): disable background (for now) on catppuccin theme

--- config/wezterm/catppuccin.lua
+++ config/wezterm/catppuccin.lua
@@ -3,40 +3,41 @@ return function(is_dark, assets)
 
   if is_dark then
     config.color_scheme = "Catppuccin Mocha"
-    config.background = {
-      {
-        source = {
-          Gradient = {
-            orientation = "Horizontal",
-            colors = {
-              "#00000C",
-              "#000026",
-              "#00000C",
-            },
-            interpolation = "CatmullRom",
-            blend = "Rgb",
-            noise = 0,
-          },
-        },
-        width = "100%",
-        height = "100%",
-        opacity = 0.75,
-      },
-      {
-        source = {
-          File = { path = assets .. "/blob_blue.gif", speed = 0.3 },
-        },
-        repeat_x = "Mirror",
-        -- width = "100%",
-        height = "100%",
-        opacity = 0.10,
-        hsb = {
-          hue = 0.9,
-          saturation = 0.9,
-          brightness = 0.8,
-        },
-      },
-    }
+    config.window_background_opacity = 0.85
+    -- config.background = {
+    --   {
+    --     source = {
+    --       Gradient = {
+    --         orientation = "Horizontal",
+    --         colors = {
+    --           "#00000C",
+    --           "#000026",
+    --           "#00000C",
+    --         },
+    --         interpolation = "CatmullRom",
+    --         blend = "Rgb",
+    --         noise = 0,
+    --       },
+    --     },
+    --     width = "100%",
+    --     height = "100%",
+    --     opacity = 0.75,
+    --   },
+    --   {
+    --     source = {
+    --       File = { path = assets .. "/blob_blue.gif", speed = 0.3 },
+    --     },
+    --     repeat_x = "Mirror",
+    --     -- width = "100%",
+    --     height = "100%",
+    --     opacity = 0.10,
+    --     hsb = {
+    --       hue = 0.9,
+    --       saturation = 0.9,
+    --       brightness = 0.8,
+    --     },
+    --   },
+    -- }
   else
     config.color_scheme = "Catppuccin Latte"
     config.window_background_opacity = 1
