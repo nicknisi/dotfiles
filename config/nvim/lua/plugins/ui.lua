98290ac1f9fa7963fa078f5eb9b9337046a134f0
Nick Nisi <nick@nisi.org>
2024-01-10 09:23:55 -0600

    fix(vim): neo-tree settings change
    follow_current_file became a table

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -299,7 +299,7 @@ return {
         },
         bind_to_cwd = true,
         cwd_target = { sidebar = "tab", current = "window" },
-        follow_current_file = true, -- This will find and focus the file in the active buffer every
+        follow_current_file = { enabled = true }, -- This will find and focus the file in the active buffer every
         -- time the current file is changed while the tree is open.
         group_empty_dirs = false, -- when true, empty folders will be grouped together
         hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
@@ -324,7 +324,7 @@ return {
         },
       },
       buffers = {
-        follow_current_file = true, -- This will find and focus the file in the active buffer every
+        follow_current_file = { enabled = true }, -- This will find and focus the file in the active buffer every
         -- time the current file is changed while the tree is open.
         group_empty_dirs = true, -- when true, empty folders will be grouped together
         show_unloaded = true,
