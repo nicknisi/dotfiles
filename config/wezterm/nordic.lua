caccb73006d2e10752427eef7350a4439b2a5d68
Nick Nisi <nick@nisi.org>
2024-01-14 11:58:21 -0600

    fix: nordic theme tweaks

--- config/wezterm/nordic.lua
+++ config/wezterm/nordic.lua
@@ -4,7 +4,42 @@ return function(is_dark, assets)
   if is_dark then
     -- config.color_scheme = "Nord (Gogh)"
     config.color_scheme = "Nocturnal Winter"
-    config.window_background_opacity = 0.90
+    config.window_background_opacity = 1
+    config.background = {
+      {
+        source = {
+          Gradient = {
+            orientation = "Horizontal",
+            colors = {
+              "#272931",
+              -- "#000026",
+              -- "#00000C",
+              "#272931",
+            },
+            interpolation = "CatmullRom",
+            blend = "Rgb",
+            noise = 0,
+          },
+        },
+        width = "100%",
+        height = "100%",
+        opacity = 0.95,
+      },
+      {
+        source = {
+          File = { path = assets .. "/blob_blue.gif", speed = 0.3 },
+        },
+        repeat_x = "Mirror",
+        -- width = "100%",
+        height = "100%",
+        opacity = 0.05,
+        hsb = {
+          hue = 0.9,
+          saturation = 0.9,
+          brightness = 0.8,
+        },
+      },
+    }
   else
     config.color_scheme = "Nord Light (Gogh)"
     config.window_background_opacity = 1
