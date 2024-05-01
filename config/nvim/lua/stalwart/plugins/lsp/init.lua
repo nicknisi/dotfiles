5bac1390e8802eca5758e44930a5d5f6858d6edf
Nick Nisi <nick@nisi.org>
2024-05-01 10:13:23 -0500

    feat: add vista.vim plugin

--- config/nvim/lua/stalwart/plugins/lsp/init.lua
+++ config/nvim/lua/stalwart/plugins/lsp/init.lua
@@ -15,6 +15,14 @@ local formatters = {
 
 return {
   {
+    "liuchengxu/vista.vim",
+    lazy = true,
+    cmd = "Vista",
+    config = function()
+      vim.g.vista_default_executive = "nvim_lsp"
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     lazy = false,
     dependencies = {
