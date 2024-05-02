4856f639b7c8d966841bf47b9e5511b742bce698
Nick Nisi <nick@nisi.org>
2024-05-02 18:00:28 -0500

    refactor: install script to handle XDG_CONFIG_DIR properly, formatting

--- install.sh
+++ install.sh
@@ -9,133 +9,160 @@ COLOR_PURPLE="\033[1;35m"
 COLOR_YELLOW="\033[1;33m"
 COLOR_NONE="\033[0m"
 
+# Configuration home
+config_home="${XDG_CONFIG_HOME:-$HOME/.config}"
+data_home="${XDG_DATA_HOME:-$HOME/.local/share}"
+
 title() {
-    echo -e "\n${COLOR_PURPLE}$1${COLOR_NONE}"
-    echo -e "${COLOR_GRAY}==============================${COLOR_NONE}\n"
+  echo -e "\n${COLOR_PURPLE}$1${COLOR_NONE}"
+  echo -e "${COLOR_GRAY}==============================${COLOR_NONE}\n"
 }
 
 error() {
-    echo -e "${COLOR_RED}Error: ${COLOR_NONE}$1"
-    exit 1
+  echo -e "${COLOR_RED}Error: ${COLOR_NONE}$1"
+  exit 1
 }
 
 warning() {
-    echo -e "${COLOR_YELLOW}Warning: ${COLOR_NONE}$1"
+  echo -e "${COLOR_YELLOW}Warning: ${COLOR_NONE}$1"
 }
 
 info() {
-    echo -e "${COLOR_BLUE}Info: ${COLOR_NONE}$1"
+  echo -e "${COLOR_BLUE}Info: ${COLOR_NONE}$1"
 }
 
 success() {
-    echo -e "${COLOR_GREEN}$1${COLOR_NONE}"
+  echo -e "${COLOR_GREEN}$1${COLOR_NONE}"
 }
 
 get_linkables() {
-    find -H "$DOTFILES" -maxdepth 3 -name '*.symlink'
+  find -H "$DOTFILES" -maxdepth 3 -name '*.symlink'
 }
 
 backup() {
-    BACKUP_DIR=$HOME/dotfiles-backup
+  BACKUP_DIR=$HOME/dotfiles-backup
 
-    echo "Creating backup directory at $BACKUP_DIR"
-    mkdir -p "$BACKUP_DIR"
+  echo "Creating backup directory at $BACKUP_DIR"
+  mkdir -p "$BACKUP_DIR"
 
-    for file in $(get_linkables); do
-        filename=".$(basename "$file" '.symlink')"
-        target="$HOME/$filename"
-        if [ -f "$target" ]; then
-            echo "backing up $filename"
-            cp "$target" "$BACKUP_DIR"
-        else
-            warning "$filename does not exist at this location or is a symlink"
-        fi
-    done
+  for file in $(get_linkables); do
+    filename=".$(basename "$file" '.symlink')"
+    target="$HOME/$filename"
+    if [ -f "$target" ]; then
+      echo "backing up $filename"
+      cp "$target" "$BACKUP_DIR"
+    else
+      warning "$filename does not exist at this location or is a symlink"
+    fi
+  done
 
-    for filename in "$HOME/.config/nvim" "$HOME/.vim" "$HOME/.vimrc"; do
-        if [ ! -L "$filename" ]; then
-            echo "backing up $filename"
-            cp -rf "$filename" "$BACKUP_DIR"
-        else
-            warning "$filename does not exist at this location or is a symlink"
-        fi
-    done
+  for filename in "$HOME/.config/nvim" "$HOME/.vim" "$HOME/.vimrc"; do
+    if [ ! -L "$filename" ]; then
+      echo "backing up $filename"
+      cp -rf "$filename" "$BACKUP_DIR"
+    else
+      warning "$filename does not exist at this location or is a symlink"
+    fi
+  done
 }
 
 
 setup_symlinks() {
-    title "Creating symlinks"
+  title "Creating symlinks"
 
-    for file in $(get_linkables) ; do
-        target="$HOME/.$(basename "$file" '.symlink')"
-        if [ -e "$target" ]; then
-            info "~${target#$HOME} already exists... Skipping."
-        else
-            info "Creating symlink for $file"
-            ln -s "$file" "$target"
-        fi
-    done
-
-    echo -e
-    info "installing to ~/.config"
-    if [ ! -d "$HOME/.config" ]; then
-        info "Creating ~/.config"
-        mkdir -p "$HOME/.config"
+  for file in $(get_linkables) ; do
+    target="$HOME/.$(basename "$file" '.symlink')"
+    if [ -e "$target" ]; then
+      info "~${target#"$HOME"} already exists... Skipping."
+    else
+      info "Creating symlink for $file"
+      ln -s "$file" "$target"
     fi
+  done
 
-    config_files=$(find "$DOTFILES/config" -maxdepth 1 2>/dev/null)
-    for config in $config_files; do
-        target="$HOME/.config/$(basename "$config")"
-        if [ -e "$target" ]; then
-            info "~${target#$HOME} already exists... Skipping."
-        else
-            info "Creating symlink for $config"
-            ln -s "$config" "$target"
-        fi
-    done
+  echo -e
+  info "installing to $config_home"
+  if [ ! -d "$config_home" ]; then
+    info "Creating $config_home"
+    mkdir -p "$config_home"
+  fi
+
+  if [ ! -d "$data_home" ]; then
+    info "Creating $data_home"
+    mkdir -p "$data_home"
+  fi
+
+  config_files=$(find "$DOTFILES/config" -maxdepth 1 2>/dev/null)
+  for config in $config_files; do
+    target="$config_home/$(basename "$config")"
+    if [ -e "$target" ]; then
+      info "~${target#"$HOME"} already exists... Skipping."
+    else
+      info "Creating symlink for $config"
+      ln -s "$config" "$target"
+    fi
+  done
+}
+
+copy() {
+  if [ ! -d "$config_home" ]; then
+    info "Creating $config_home"
+    mkdir -p "$config_home"
+  fi
+
+  if [ ! -d "$data_home" ]; then
+    info "Creating $data_home"
+    mkdir -p "$data_home"
+  fi
+  config_files=$(find "$DOTFILES/config" -maxdepth 1 2>/dev/null)
+  for config in $config_files; do
+    target="$config_home/$(basename "$config")"
+    info "copying $config to $config_home/$config"
+    cp -R "$config" "$target"
+  done
 }
 
 setup_git() {
-    title "Setting up Git"
+  title "Setting up Git"
 
-    defaultName=$(git config user.name)
-    defaultEmail=$(git config user.email)
-    defaultGithub=$(git config github.user)
+  defaultName=$(git config user.name)
+  defaultEmail=$(git config user.email)
+  defaultGithub=$(git config github.user)
 
-    read -rp "Name [$defaultName] " name
-    read -rp "Email [$defaultEmail] " email
-    read -rp "Github username [$defaultGithub] " github
+  read -rp "Name [$defaultName] " name
+  read -rp "Email [$defaultEmail] " email
+  read -rp "Github username [$defaultGithub] " github
 
-    git config -f ~/.gitconfig-local user.name "${name:-$defaultName}"
-    git config -f ~/.gitconfig-local user.email "${email:-$defaultEmail}"
-    git config -f ~/.gitconfig-local github.user "${github:-$defaultGithub}"
+  git config -f ~/.gitconfig-local user.name "${name:-$defaultName}"
+  git config -f ~/.gitconfig-local user.email "${email:-$defaultEmail}"
+  git config -f ~/.gitconfig-local github.user "${github:-$defaultGithub}"
 
-    if [[ "$(uname)" == "Darwin" ]]; then
-        git config --global credential.helper "osxkeychain"
+  if [[ "$(uname)" == "Darwin" ]]; then
+    git config --global credential.helper "osxkeychain"
+  else
+    read -rn 1 -p "Save user and password to an unencrypted file to avoid writing? [y/N] " save
+    if [[ $save =~ ^([Yy])$ ]]; then
+      git config --global credential.helper "store"
     else
-        read -rn 1 -p "Save user and password to an unencrypted file to avoid writing? [y/N] " save
-        if [[ $save =~ ^([Yy])$ ]]; then
-            git config --global credential.helper "store"
-        else
-            git config --global credential.helper "cache --timeout 3600"
-        fi
+      git config --global credential.helper "cache --timeout 3600"
     fi
+  fi
 }
 
 setup_homebrew() {
-    title "Setting up Homebrew"
+  title "Setting up Homebrew"
 
-    if test ! "$(command -v brew)"; then
-        info "Homebrew not installed. Installing."
-        # Run as a login shell (non-interactive) so that the script doesn't pause for user input
-        curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash --login
-    fi
+  if test ! "$(command -v brew)"; then
+    info "Homebrew not installed. Installing."
+    # Run as a login shell (non-interactive) so that the script doesn't pause for user input
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash --login
+  fi
 
-    if [ "$(uname)" == "Linux" ]; then
-        test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-        test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-        test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-    fi
+  if [ "$(uname)" == "Linux" ]; then
+    test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+    test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+  fi
 
     # install brew dependencies from Brewfile
     brew bundle
@@ -144,30 +171,30 @@ setup_homebrew() {
     echo -e
     info "Installing fzf"
     "$(brew --prefix)"/opt/fzf/install --key-bindings --completion --no-update-rc --no-bash --no-fish
-}
+  }
 
-fetch_catppuccin_theme() {
+  fetch_catppuccin_theme() {
     for palette in frappe latte macchiato mocha; do
-        curl -o "$DOTFILES/config/kitty/themes/catppuccin-$palette.conf" "https://raw.githubusercontent.com/catppuccin/kitty/main/$palette.conf"
+      curl -o "$DOTFILES/config/kitty/themes/catppuccin-$palette.conf" "https://raw.githubusercontent.com/catppuccin/kitty/main/$palette.conf"
     done
-}
+  }
 
-setup_shell() {
+  setup_shell() {
     title "Configuring shell"
 
     [[ -n "$(command -v brew)" ]] && zsh_path="$(brew --prefix)/bin/zsh" || zsh_path="$(which zsh)"
     if ! grep "$zsh_path" /etc/shells; then
-        info "adding $zsh_path to /etc/shells"
-        echo "$zsh_path" | sudo tee -a /etc/shells
+      info "adding $zsh_path to /etc/shells"
+      echo "$zsh_path" | sudo tee -a /etc/shells
     fi
 
     if [[ "$SHELL" != "$zsh_path" ]]; then
-        chsh -s "$zsh_path"
-        info "default shell changed to $zsh_path"
+      chsh -s "$zsh_path"
+      info "default shell changed to $zsh_path"
     fi
-}
+  }
 
-function setup_terminfo() {
+  function setup_terminfo() {
     title "Configuring terminfo"
 
     info "adding tmux.terminfo"
@@ -175,103 +202,106 @@ function setup_terminfo() {
 
     info "adding xterm-256color-italic.terminfo"
     tic -x "$DOTFILES/resources/xterm-256color-italic.terminfo"
-}
+  }
 
-setup_macos() {
+  setup_macos() {
     title "Configuring macOS"
     if [[ "$(uname)" == "Darwin" ]]; then
 
-        echo "Finder: show all filename extensions"
-        defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+      echo "Finder: show all filename extensions"
+      defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-        echo "show hidden files by default"
-        defaults write com.apple.Finder AppleShowAllFiles -bool false
+      echo "show hidden files by default"
+      defaults write com.apple.Finder AppleShowAllFiles -bool false
 
-        echo "only use UTF-8 in Terminal.app"
-        defaults write com.apple.terminal StringEncodings -array 4
+      echo "only use UTF-8 in Terminal.app"
+      defaults write com.apple.terminal StringEncodings -array 4
 
-        echo "expand save dialog by default"
-        defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+      echo "expand save dialog by default"
+      defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 
-        echo "show the ~/Library folder in Finder"
-        chflags nohidden ~/Library
+      echo "show the ~/Library folder in Finder"
+      chflags nohidden ~/Library
 
-        echo "Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
-        defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+      echo "Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
+      defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
-        echo "Enable subpixel font rendering on non-Apple LCDs"
-        defaults write NSGlobalDomain AppleFontSmoothing -int 2
+      echo "Enable subpixel font rendering on non-Apple LCDs"
+      defaults write NSGlobalDomain AppleFontSmoothing -int 2
 
-        echo "Use current directory as default search scope in Finder"
-        defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+      echo "Use current directory as default search scope in Finder"
+      defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
-        echo "Show Path bar in Finder"
-        defaults write com.apple.finder ShowPathbar -bool true
+      echo "Show Path bar in Finder"
+      defaults write com.apple.finder ShowPathbar -bool true
 
-        echo "Show Status bar in Finder"
-        defaults write com.apple.finder ShowStatusBar -bool true
+      echo "Show Status bar in Finder"
+      defaults write com.apple.finder ShowStatusBar -bool true
 
-        echo "Disable press-and-hold for keys in favor of key repeat"
-        defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+      echo "Disable press-and-hold for keys in favor of key repeat"
+      defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
-        echo "Set a blazingly fast keyboard repeat rate"
-        defaults write NSGlobalDomain KeyRepeat -int 1
+      echo "Set a blazingly fast keyboard repeat rate"
+      defaults write NSGlobalDomain KeyRepeat -int 1
 
-        echo "Set a shorter Delay until key repeat"
-        defaults write NSGlobalDomain InitialKeyRepeat -int 15
+      echo "Set a shorter Delay until key repeat"
+      defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
-        echo "Enable tap to click (Trackpad)"
-        defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+      echo "Enable tap to click (Trackpad)"
+      defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 
-        echo "Enable Safari’s debug menu"
-        defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+      echo "Enable Safari’s debug menu"
+      defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 
-        echo "Kill affected applications"
+      echo "Kill affected applications"
 
-        for app in Safari Finder Dock Mail SystemUIServer; do killall "$app" >/dev/null 2>&1; done
+      for app in Safari Finder Dock Mail SystemUIServer; do killall "$app" >/dev/null 2>&1; done
     else
-        warning "macOS not detected. Skipping."
+      warning "macOS not detected. Skipping."
     fi
-}
+  }
 
-case "$1" in
+  case "$1" in
     backup)
-        backup
-        ;;
+      backup
+      ;;
     link)
-        setup_symlinks
-        ;;
+      setup_symlinks
+      ;;
+    copy)
+      copy
+      ;;
     git)
-        setup_git
-        ;;
+      setup_git
+      ;;
     homebrew)
-        setup_homebrew
-        ;;
+      setup_homebrew
+      ;;
     shell)
-        setup_shell
-        ;;
+      setup_shell
+      ;;
     terminfo)
-        setup_terminfo
-        ;;
+      setup_terminfo
+      ;;
     macos)
-        setup_macos
-        ;;
+      setup_macos
+      ;;
     catppuccin)
-        fetch_catppuccin_theme
-        ;;
+      fetch_catppuccin_theme
+      ;;
     all)
-        setup_symlinks
-        setup_terminfo
-        setup_homebrew
-        setup_shell
-        setup_git
-        setup_macos
-        ;;
+      setup_symlinks
+      setup_terminfo
+      setup_homebrew
+      setup_shell
+      setup_git
+      setup_macos
+      ;;
     *)
-        echo -e $"\nUsage: $(basename "$0") {backup|link|git|homebrew|shell|terminfo|macos|all}\n"
-        exit 1
-        ;;
-esac
+      echo -e $"\nUsage: $(basename "$0") {backup|link|git|homebrew|shell|terminfo|macos|all}\n"
+      exit 1
+      ;;
+  esac
 
-echo -e
-success "Done."
+  echo -e
+  success "Done."
