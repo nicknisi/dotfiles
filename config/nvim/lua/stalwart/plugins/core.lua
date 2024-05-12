61f25f4d4f9ebe134699962a52553d2634420acb
Nick Nisi <nick@nisi.org>
2024-05-11 22:09:11 -0500

    feat: add dropbar.nvim plugin

--- config/nvim/lua/stalwart/plugins/core.lua
+++ config/nvim/lua/stalwart/plugins/core.lua
@@ -62,4 +62,11 @@ return {
       -- refer to the configuration section below
     },
   },
+  {
+    "Bekaboo/dropbar.nvim",
+    -- optional, but required for fuzzy finder support
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+    },
+  },
 }
