3d7351481304e99245b3d5dd4101ab5443f37899
Nick Nisi <nick@nisi.org>
2024-07-05 14:52:57 -0500

    fix: load latest version of lazy

--- config/nvim/lua/nisi/init.lua
+++ config/nvim/lua/nisi/init.lua
@@ -61,15 +61,18 @@ for _, path in ipairs(paths) do
 end
 
 local function load_lazy(path)
-  if not vim.loop.fs_stat(path) then
-    vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/folke/lazy.nvim.git",
-      "--branch=stable",
-      path,
-    })
+  if not (vim.uv or vim.loop).fs_stat(path) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, path })
+    if vim.v.shell_error ~= 0 then
+      vim.api.nvim_echo({
+        { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+        { out, "WarningMsg" },
+        { "\nPress any key to exit..." },
+      }, true, {})
+      vim.fn.getchar()
+      os.exit(1)
+    end
   end
   vim.opt.rtp:prepend(path)
 end
