1abdf6f070bca1555fe286e94b619f93b1b5d59c
Nick Nisi <nick@nisi.org>
2024-07-16 20:25:43 -0500

    nit(vim): move neo-tree back to the left side
    I tried.

--- config/nvim/lua/nisi/plugins/files.lua
+++ config/nvim/lua/nisi/plugins/files.lua
@@ -97,7 +97,7 @@ return {
         },
       },
       window = {
-        position = "right",
+        position = "left",
         width = 40,
         mapping_options = { noremap = true, nowait = true },
         mappings = {
