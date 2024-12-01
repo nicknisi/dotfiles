760680751887826946d929b50c11115d270701ff
wl_ubuntu <wl_ubuntu@Wl.com>
2024-12-01 17:24:02 +0800

    Enable `inlay_hints`

--- config/nvim/lua/plugins/astrolsp.lua
+++ config/nvim/lua/plugins/astrolsp.lua
@@ -12,7 +12,7 @@ return {
         -- Configuration table of features provided by AstroLSP
         features = {
             codelens = true, -- enable/disable codelens refresh on start
-            inlay_hints = false, -- enable/disable inlay hints on start
+            inlay_hints = true, -- enable/disable inlay hints on start
             semantic_tokens = true, -- enable/disable semantic token highlighting
         },
         -- customize lsp formatting options
