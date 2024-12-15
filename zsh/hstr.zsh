e0288e15963e28459a2db28dbaf4ab4a0745f01d
wl_ubuntu <wl_ubuntu@Wl.com>
2024-12-15 18:44:29 +0800

    Move the position of prompt to bottom in hstr

--- zsh/hstr.zsh
+++ zsh/hstr.zsh
@@ -1,7 +1,8 @@
 # HSTR configuration - add this to ~/.zshrc
 alias hh=hstr                    # hh to be alias for hstr
 setopt histignorespace           # skip cmds w/ leading space from history
-export HSTR_CONFIG=hicolor       # get more colors
 bindkey -s "\C-r" "\C-a hstr -- \C-j"     # bind hstr to Ctrl-r (for Vi mode check doc)
+
+# https://github.com/dvorka/hstr/blob/master/CONFIGURATION.md#hstr-config-options
+export HSTR_CONFIG=raw-history-view,prompt-bottom
 export HSTR_TIOCSTI=y
-export HSTR_CONFIG=raw-history-view
