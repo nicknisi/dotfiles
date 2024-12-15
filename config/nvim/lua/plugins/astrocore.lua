f5b1ab965ec2b178a993d6fdf57d0b2d0fb99261
wl_ubuntu <wl_ubuntu@Wl.com>
2024-12-15 19:29:09 +0800

    Add the jumplist-stack setting for jump

--- config/nvim/lua/plugins/astrocore.lua
+++ config/nvim/lua/plugins/astrocore.lua
@@ -10,7 +10,7 @@ return {
     opts = {
         -- Configure core features of AstroNvim
         features = {
-            large_buf = { size = 1024 * 500, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+            large_buf = { size = 1024 * 5000, lines = 80000 }, -- set global limits for large files for disabling features like treesitter
             autopairs = true, -- enable autopairs at start
             cmp = true, -- enable completion at start
             diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
@@ -39,6 +39,7 @@ return {
                 writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
                 expandtab = true, -- convert tabs to spaces
                 foldcolumn = "0",
+                jumpoptions = "view",
             },
             g = { -- vim.g.<key>
                 -- configure global vim variables (vim.g)
