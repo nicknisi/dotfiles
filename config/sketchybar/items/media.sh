afd69024d58d4c8f3bc0ad5f8e5a903be30b9c1e
Nick Nisi <nick@nisi.org>
2024-12-16 19:28:21 -0600

    updates to scripts for sketchybar

--- config/sketchybar/items/media.sh
+++ config/sketchybar/items/media.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-sketchybar --add item media e \
-  --set media label.color="$ACCENT_COLOR" \
-  label.max_chars=20 \
-  icon.padding_left=0 \
-  scroll_texts=on \
-  icon=􀑪 \
-  icon.color="$ACCENT_COLOR" \
-  background.drawing=off \
-  script="$PLUGIN_DIR/media.sh" \
-  --subscribe media media_change
+media=(
+  icon.background.drawing=on
+  icon.background.image=media.artwork
+  icon.background.image.corner_radius=9
+  script="$PLUGIN_DIR/media.sh"
+  label.max_chars=15
+  scroll_texts=on
+  updates=on
+)
+
+sketchybar --add item media center \
+           --set media "${media[@]}" \
+           --subscribe media media_change
