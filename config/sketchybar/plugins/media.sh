487e64d940d9818de59e124dc5cdeb3851e7fd0c
Nick Nisi <nick@nisi.org>
2024-12-16 19:28:21 -0600

    updates to scripts for sketchybar

--- config/sketchybar/plugins/media.sh
+++ config/sketchybar/plugins/media.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-STATE="$(echo "$INFO" | jq -r '.state')"
-if [ "$STATE" = "playing" ]; then
-  MEDIA="$(echo "$INFO" | jq -r '.title + " - " + .artist')"
-  sketchybar --set "$NAME" label="$MEDIA" drawing=on
-else
-  sketchybar --set "$NAME" drawing=off
-fi
+update_media() {
+  STATE="$(echo "$INFO" | jq -r '.state')"
+
+  if [ "$STATE" = "playing" ]; then
+    APP=$(echo "$INFO" | jq -r '.app')
+    MEDIA="$(echo "$INFO" | jq -r '.title + " - " + .artist')"
+    sketchybar --set "$NAME" label="$MEDIA" drawing=on
+  else
+    sketchybar --set "$NAME" drawing=off
+  fi
+}
+
+case "$SENDER" in
+  "media_change") update_media
+  ;;
+esac
