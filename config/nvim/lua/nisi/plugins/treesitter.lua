038bc9c819fc29d2f75ab31589901f8708027a6e
Nick Nisi <nick@nisi.org>
2024-12-18 11:07:04 -0600

    feat(vim): add support for blade templates (treesitter)

--- config/nvim/lua/nisi/plugins/treesitter.lua
+++ config/nvim/lua/nisi/plugins/treesitter.lua
@@ -15,6 +15,21 @@ return {
       end
       require("lazy.core.loader").add_to_rtp(plugin)
       require("nvim-treesitter.query_predicates")
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.blade = {
+        install_info = {
+          url = "https://github.com/EmranMR/tree-sitter-blade",
+          files = { "src/parser.c" },
+          branch = "main",
+        },
+        filetype = "blade",
+      }
+
+      vim.filetype.add({
+        pattern = {
+          [".*%.blade%.php"] = "blade",
+        },
+      })
     end,
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
@@ -24,6 +39,7 @@ return {
       ensure_installed = {
         "astro",
         "bash",
+        "blade",
         "c",
         "comment",
         "cpp",
