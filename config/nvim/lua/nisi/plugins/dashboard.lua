59a32b9dde047bde3d0ea4a8a067dc42b4f66c10
Nick Nisi <nick@nisi.org>
2025-01-19 13:46:54 -0600

    feat(vim): introduce snacks.nvim and replace dashboard

---
+++ config/nvim/lua/nisi/plugins/dashboard.lua
@@ -0,0 +1,107 @@
+local config = require("nisi").config
+local ascii = require("nisi.assets").ascii
+local utils = require("nisi.utils")
+
+local group = vim.api.nvim_create_augroup("Startup", { clear = true })
+vim.api.nvim_create_autocmd("FileType", { group = group, pattern = "startup", command = "setlocal list&" })
+
+-- if in a git directory, open git files, otherwise open all files when pressing the "Find File" shortcut
+local find_command = utils.is_in_git_repo() and ":Telescope git_files" or ":Telescope find_files"
+
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    dashboard = {
+      enabled = true,
+      row = nil,
+      col = nil,
+      preset = {
+        header = table.concat(ascii.nicknisi, "\n"),
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = " ", key = "t", desc = "Find File", action = find_command },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+          },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+      sections = {
+        { section = "header" },
+        { pane = 2, section = "keys" },
+        {
+          pane = 2,
+          icon = " ",
+          desc = "Browse Repo",
+          padding = 1,
+          key = "b",
+          action = function()
+            Snacks.gitbrowse()
+          end,
+        },
+        function()
+          local in_git = Snacks.git.get_root() ~= nil
+          local cmds = {
+            {
+              title = "Notifications",
+              cmd = "gh notify -s -a -n5",
+              action = function()
+                vim.ui.open("https://github.com/notifications")
+              end,
+              key = "n",
+              icon = " ",
+              height = 5,
+              enabled = false,
+            },
+            {
+              title = "Open Issues",
+              cmd = "gh issue list -L 3",
+              key = "i",
+              action = function()
+                vim.fn.jobstart("gh issue list --web", { detach = true })
+              end,
+              icon = " ",
+              height = 7,
+            },
+            {
+              icon = " ",
+              title = "Open PRs",
+              cmd = "gh pr list -L 3",
+              key = "p",
+              action = function()
+                vim.fn.jobstart("gh pr list --web", { detach = true })
+              end,
+              height = 7,
+            },
+            {
+              icon = " ",
+              title = "Git Status",
+              cmd = "git --no-pager diff --stat -B -M -C",
+              height = 10,
+            },
+          }
+          return vim.tbl_map(function(cmd)
+            return vim.tbl_extend("force", {
+              pane = 2,
+              section = "terminal",
+              enabled = in_git,
+              padding = 1,
+              ttl = 5 * 60,
+              indent = 3,
+            }, cmd)
+          end, cmds)
+        end,
+        { section = "startup" },
+      },
+    },
+  },
+}
