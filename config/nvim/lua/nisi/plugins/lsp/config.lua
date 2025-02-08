88b75d4b316665746c95addc681f2ebdf1dd5903
Nick Nisi <nick@nisi.org>
2025-02-08 07:55:22 -0600

    feat(vim): add colored diagnostic line nubers

--- config/nvim/lua/nisi/plugins/lsp/config.lua
+++ config/nvim/lua/nisi/plugins/lsp/config.lua
@@ -284,6 +284,23 @@ function M.setup()
     end
   end
 
+  vim.diagnostic.config({
+    signs = {
+      text = {
+        [vim.diagnostic.severity.ERROR] = "",
+        [vim.diagnostic.severity.WARN] = "",
+        [vim.diagnostic.severity.INFO] = "",
+        [vim.diagnostic.severity.HINT] = "",
+      },
+      numhl = {
+        [vim.diagnostic.severity.WARN] = "WarningMsg",
+        [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+        [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+        [vim.diagnostic.severity.HINT] = "DiagnosticHint",
+      },
+    },
+  })
+
   mason_lspconfig.setup_handlers(handlers)
 end
 
