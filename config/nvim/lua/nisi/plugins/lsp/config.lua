c71ddc792d79f369ff72e98e0fd0c8ee6296f0d4
Nick Nisi <nick@nisi.org>
2025-02-13 10:25:04 -0600

    feat(vim): automatically setup schemas for known JSON files

--- config/nvim/lua/nisi/plugins/lsp/config.lua
+++ config/nvim/lua/nisi/plugins/lsp/config.lua
@@ -201,6 +201,65 @@ function M.setup()
     end
   end
 
+  if utils.exists_in_table(servers, "jsonls") then
+    handlers["jsonls"] = function()
+      lspconfig.jsonls.setup(make_conf({
+        cmd = { "vscode-json-language-server", "--stdio" },
+        filetypes = { "json", "jsonc" },
+        settings = {
+          json = {
+            schemas = {
+              {
+                fileMatch = { "package.json" },
+                url = "https://json.schemastore.org/package.json",
+              },
+              {
+                fileMatch = { "manifest.json", "manifest.webmanifest" },
+                url = "https://json.schemastore.org/web-manifest-combined.json",
+              },
+              {
+                fileMatch = { "tsconfig*.json" },
+                url = "https://json.schemastore.org/tsconfig.json",
+              },
+              {
+                fileMatch = {
+                  ".prettierrc",
+                  ".prettierrc.json",
+                  "prettier.config.json",
+                },
+                url = "https://json.schemastore.org/prettierrc.json",
+              },
+              {
+                fileMatch = { ".eslintrc", ".eslintrc.json" },
+                url = "https://json.schemastore.org/eslintrc.json",
+              },
+              {
+                fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
+                url = "https://json.schemastore.org/babelrc.json",
+              },
+              {
+                fileMatch = { "lerna.json" },
+                url = "https://json.schemastore.org/lerna.json",
+              },
+              {
+                fileMatch = { "now.json", "vercel.json" },
+                url = "https://json.schemastore.org/now.json",
+              },
+              {
+                fileMatch = {
+                  ".stylelintrc",
+                  ".stylelintrc.json",
+                  "stylelint.config.json",
+                },
+                url = "http://json.schemastore.org/stylelintrc.json",
+              },
+            },
+          },
+        },
+      }))
+    end
+  end
+
   if utils.exists_in_table(servers, "denols") then
     handlers["denols"] = function()
       lspconfig.denols.setup(make_conf({
